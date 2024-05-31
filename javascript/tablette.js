ce94276e599112dc47834fe8e3b3945f21632812
Emmanuel El-Agha <2278542@bdeb.qc.ca>
2024-05-31 11:48:21 -0400

    Supprimation de la deuxième version du code javascript du fichier tablette.js

--- javascript/tablette.js
+++ javascript/tablette.js
@@ -1,5 +1,4 @@
 
-//vers 1   ??? 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('http://localhost:8080/ords/RESTSCOTT/tablette') 
         .then(response => response.json())
@@ -17,29 +16,3 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.log('Erreur: ', error));
 });
 
-
-
-
-//vers 2        ???
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://localhost:8080/ords/RESTSCOTT/tablette')
-        .then(response => response.json())
-        .then(data => {
-            let output = '';
-            data.forEach(tablette => {
-                output += `
-                    <div class="tablette">
-                        <h3>${tablette.marque} ${tablette.numero}</h3>
-                        <p>Marque: ${tablette.marque}</p>
-                        <p>Couleur: ${tablette.couleur}</p>
-                        <p>Prix: ${tablette.prix}€</p>
-                        <p>Caméras: ${tablette.cameras}</p>
-                        <p>Description: ${tablette.description}</p>
-                    </div>
-                `;
-
-            });
-            document.getElementById('tablettes-container').innerHTML = output;
-        })
-        .catch(error => console.log('Erreur: ', error));
-});
